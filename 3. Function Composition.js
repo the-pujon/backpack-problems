0b7751dc3f2099d9aad7e68954d277373284e308
Pujon Das Auvi <auvipujondas@gmail.com>
2024-11-02 04:23:01 +0600

    Function Composition solved

---
+++ 3. Function Composition.js
@@ -0,0 +1,25 @@
+//3. Function Composition
+//Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
+
+const square = (num) => num * num;
+const double = (num) => num * 2;
+const addFive = (num) => num + 5;
+
+// Compose function using map
+//const composeWithMap = (...fns) => (x) => {
+//    let result = x;
+//    fns.map(fn => {
+//      result = fn(result);
+//    });
+//    return result;
+//  };
+
+// Compose function using reduce
+const compose =
+  (...fns) =>
+  (x) =>
+    fns.reduce((acc, fn) => fn(acc), x);
+
+const composedFunction = compose(square, double, addFive);
+
+console.log(composedFunction(3)); // Output: 38
